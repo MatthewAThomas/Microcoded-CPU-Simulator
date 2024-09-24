d9ee8ee2e6ecd347c08a99d37436dba632439e2f
Matthew Thomas <matthew.a.thomas2002@gmail.com>
2024-09-24 06:09:01 +0000

    Seems to be compiling correctly: test-add-ld-st.txt executes as expexted.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     if (argc == 2)
         program = argv[1];
     if (argc > 2) {
-        printf("Too many arguments provided to emulator\n");
+        printf("Too many arguments provided to simulator\n");
         return -1;
     }
 
